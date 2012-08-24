24c9114dd58a71128f93bce2a9f0f97e9d1886f6
Paul Dixon <paul.dixon@mintbridge.co.uk>
2012-08-24 20:11:40 +0100

    Initial implementation of grunt-behat

---
+++ README.md
@@ -0,0 +1,58 @@
+# grunt-behat
+
+A [grunt]: https://github.com/cowboy/grunt task to run your [Behat]: http://behat.org/ BDD tests
+
+## Getting Started
+Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-behat`
+
+Then add this line to your project's `grunt.js` gruntfile:
+
+```javascript
+grunt.loadNpmTasks('grunt-behat');
+```
+
+[grunt]: https://github.com/cowboy/grunt
+[getting_started]: https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
+
+## Documentation
+This task is a [multi task][types_of_tasks], meaning that grunt will automatically iterate over all `behat` targets if a target is not specified.
+
+###Example Config
+
+```javascript
+grunt.initConfig({
+  behat: {
+    example: {
+      options: {
+        output: false,
+        failOnUndefined: false,
+        failOnFailed: false
+      },
+      features: 'features/',
+      flags: '-f pretty'
+    },
+    example2: {
+      features: 'features/',
+    }
+  }
+});
+```
+
+####Features
+Set this to be the path to your features directory
+
+####Options
+Options can be used to set when the task should fail and the output that will be displayed when its does.
+
+####Flags
+The flags can be used to configure Behat, see http://docs.behat.org/guides/6.cli.html for the flags that behat accepts.
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+
+## Release History
+*   __24/08/2012 - 0.1.0__: Initial release.
+
+## License
+Copyright (c) 2012 Paul Dixon  
+Licensed under the MIT license.
---
+++ tasks/behat.js
@@ -0,0 +1,127 @@
+/*
+ * grunt-behat
+ * https://github.com/mintbridge/grunt-behat
+ *
+ * Copyright (c) 2012 Paul Dixon
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  // ==========================================================================
+  // TASKS
+  // ==========================================================================
+
+  var cp = require('child_process');
+
+  grunt.registerMultiTask('behat', 'Run your Behat BDD tests', function() {
+    // Get target-specific options override the default options.
+    var options, globals, tmp;
+
+    tmp = grunt.config(['behat', this.target, 'options']);
+    if (typeof tmp === 'object') {
+      grunt.verbose.writeln('Using "' + this.target + '" Behat options.');
+      options = tmp;
+    } else {
+      grunt.verbose.writeln('Using master Behat options.');
+      options = grunt.config('behat.options');
+    }
+    grunt.verbose.writeflags(options, 'Options'); 
+
+    var data = {
+      cmd: 'behat',
+      args: [],
+      options: options
+    }
+
+    if(typeof this.data.flags !== 'undefined') {
+      data.args.push(this.data.flags);
+    }
+    if(typeof this.data.features !== 'undefined') {
+      data.args.push(this.data.features);
+    }
+
+    var done = this.async();
+
+    grunt.helper('behat', data, function(err, res) {
+      if (err) {
+        grunt.warn(err);
+        done(false);
+
+        return;
+      }
+
+      done();
+    });
+  });
+
+  function hasUndefinedSteps(res){
+    var regexp = new RegExp("undefined");
+    return regexp.test(res);
+  }
+
+  function hasFailedSteps(res){
+    var regexp = new RegExp("failed");
+    return regexp.test(res);
+  }
+  // ==========================================================================
+  // HELPERS
+  // ==========================================================================
+
+  grunt.registerHelper('behat', function(data, callback) {
+
+    var spawn = require('child_process').spawn,
+    behat     = spawn(data.cmd, data.args),
+    stderr    = '',
+    stdout    = '',
+    options   = data.options || {};
+
+    if(typeof options.failOnUndefined === 'undefined') {
+      options.failOnUndefined = false;
+    }
+    if(typeof options.failOnFailed === 'undefined') {
+      options.failOnFailed = true;
+    }
+
+    behat.stdout.on('data', function (data) {
+      stdout += data;
+    });
+
+    behat.stderr.on('data', function (data) {
+      stderr += data;
+    });
+
+    behat.on('exit', function (code) {
+      if (code === 127) {
+        grunt.log.errorlns(
+          'In order for this task to work properly, Behat must be ' +
+          'installed and in the system PATH (if you can run "behat" at' +
+          ' the command line, this task should work). Unfortunately, ' +
+          'Behat cannot be installed automatically via npm or grunt. ' +
+          'See the Behat installation instructions: ' +
+          'http://docs.behat.org/quick_intro.html#installation'
+        );
+        grunt.warn('Behat not found.', code);
+      }
+      else {
+        if(options.failOnUndefined && hasUndefinedSteps(stdout)) {
+          grunt.verbose.writeln(stdout);
+          stderr = 'Undefined Steps';
+          if(options.output) {
+            stderr = stdout;
+          }
+        }
+        if(options.failOnFailed && hasFailedSteps(stdout)) {
+          grunt.verbose.writeln(stdout);
+          stderr = 'Failed Steps';
+          if(options.output) {
+            stderr = stdout;
+          }
+        }
+      }
+      callback(stderr, stdout);
+    });
+
+  });
+
+};
